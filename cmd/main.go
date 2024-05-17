f729a213b4c88d78bb73eafbe1d892b277619b56
Daniel R <nzdanron@gmail.com>
2024-05-18 11:58:59 +1200

    chore: project setup
    "Hello, world!" serving http server

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Page struct {}
+
+func newPage() Page {
+    return Page {}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = newTemplates()
+
+    page := newPage()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", page)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
